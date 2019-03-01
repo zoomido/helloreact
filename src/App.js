af188acc61bd2d43366ecd4cb663ffebc6959730
unknown <am@internet.se>
2019-03-01 17:01:38 +0100

    Ran prettier

--- src/App.js
+++ src/App.js
@@ -25,30 +25,32 @@ class App extends Component {
   };
 
   // Toggle Complete
-  markComplete = (id) => {
+  markComplete = id => {
     this.setState({
       todos: this.state.todos.map(todo => {
         if (todo.id === id) {
-          todo.completed = !todo.completed
+          todo.completed = !todo.completed;
         }
         return todo;
       })
     });
-  }
+  };
 
   // Delete Todo
-  delTodo = (id) => {
+  delTodo = id => {
     this.setState({
       todos: [...this.state.todos.filter(todo => todo.id !== id)]
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos}
+        <Todos
+          todos={this.state.todos}
           markComplete={this.markComplete}
-          delTodo={this.delTodo} />
+          delTodo={this.delTodo}
+        />
       </div>
     );
   }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
-import TodoItem from './TodoItem';
-import PropTypes from 'prop-types';
+import TodoItem from "./TodoItem";
+import PropTypes from "prop-types";
 
 class Todos extends Component {
-
   render() {
-    return this.props.todos.map((todo) => (
-      <TodoItem key={todo.id} todo={todo} markComplete=
-        {this.props.markComplete} delTodo={this.props.delTodo} />
+    return this.props.todos.map(todo => (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        markComplete={this.props.markComplete}
+        delTodo={this.props.delTodo}
+      />
     ));
   }
 }
@@ -15,6 +18,6 @@ class Todos extends Component {
 // PropTypes
 Todos.propTypes = {
   todos: PropTypes.array.isRequired
-}
+};
 
 export default Todos;
